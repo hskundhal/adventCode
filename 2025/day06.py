91e409e330acf9b38c14218fa6169e3641532b10
harpreetsingh <hskundhal@gmail.com>
2025-12-06 23:08:05 -0500

    day06

--- 2025/day06.py
+++ 2025/day06.py
@@ -4,16 +4,16 @@ def part1():
     grid=[]
     result = 0
     for line in lines:
-        line = line.strip()
+        line = line.rstrip('\n')
         line = line.split()
         grid.append(line)
 
     for i,sign in enumerate(grid[-1]):
-        print(sign , "col" ,i)
+        # print(sign , "col" ,i)
         varResult = int(grid[0][i])
         for row in grid[1:-1]:
             varResult = eval(str(varResult)+sign+row[i])
-        print (varResult)
+        # print (varResult)
         result += varResult
     print("result ",result)
 
@@ -23,13 +23,44 @@ def part1():
 def part2():
     with open("input6.txt", "r") as f:
         lines = f.readlines()
+
+    detailgrid = []
+    result = 0
     for line in lines:
-        line = line.strip()
-        print(line)
+        line = line.rstrip('\n')
+        detailgrid.append(list(line))
+
+    transposedetailgrid = [list(row) for row in zip(*detailgrid)]
+    
+    if transposedetailgrid[0][-1] != " ":
+        sign = transposedetailgrid[0][-1]
+
+    startpos = 0
+    signResult = int("".join(transposedetailgrid[startpos][:-1]))
+    while startpos < len(transposedetailgrid)-1:
+        startpos += 1
+        if ("".join(transposedetailgrid[startpos])).strip() == "" and startpos < len(transposedetailgrid)-1:
+            result = result + signResult
+            signResult = int("".join(transposedetailgrid[startpos+1][:-1]))
+            if signResult == " ":
+                print("haye tooba")
+            sign = transposedetailgrid[startpos+1][-1]
+            startpos += 2
+        if startpos >= len(transposedetailgrid) :
+            break
+
+        nextline = "".join(transposedetailgrid[startpos][:-1])
+        # print(str(signResult) + sign + nextline, startpos)
+        signResult= eval(str(signResult) + sign + nextline)
+
+    print(result+signResult)
+
+
+
 
 
 print("--- Part 1 ---")
 part1()
-
-# print("\n--- Part 2 ---")
-# part2()
+#
+print("\n--- Part 2 ---")
+part2()
